7bc2658baadd1402e992c56b2c50a41e2fa9319d
Bhanu <bhanurorkitchen2@gmail.com>
2013-03-09 13:52:09 +0100

    Entry spec

--- spec/models/entry_spec.rb
+++ spec/models/entry_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Entry do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:user)}
+  it {should belong_to(:contest)}
+
+  it {should validate_presence_of(:contest)}
+  it {should validate_presence_of(:user)}
+  it {should validate_presence_of(:description)}
+  it {should validate_presence_of(:data_set_url)}
+  it {should ensure_inclusion_of(:rating).in_array(1..5) }
+
+  describe '.update_rating' do
+    let(:entry) { create(:entry) }
+    let(:new_rating) { 5 }
+    let(:invalid_rating) { 7 }
+    it "should set valid rating of entry" do
+      expect(entry.update_rating(new_rating)).to be_true
+      expect(entry.rating).to eq(new_rating)
+    end
+
+    it "should not set valid rating of entry" do
+      expect(entry.update_rating(invalid_rating)).to be_false
+      expect(entry.reload.rating).not_to eq(invalid_rating)
+    end
+  end
 end
