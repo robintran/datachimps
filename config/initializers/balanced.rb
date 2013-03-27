9b0bdb5c2621e764c309983070b4d0c48f449058
Prasad Madala <bhanuprasad143@gmail.com>
2013-03-27 12:33:49 +0700

    fix balanced payments api

--- config/initializers/balanced.rb
+++ config/initializers/balanced.rb
@@ -1,2 +1,3 @@
 # Balanced.configure(ENV['BALANCED_API_SECRET'])
-Balanced.configure('099e55e07f7311e2b923026ba7c1aba6')
+# Balanced.configure('099e55e07f7311e2b923026ba7c1aba6')
+Balanced.configure('fca21900969511e2a138026ba7c1aba6')
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320153440) do
+ActiveRecord::Schema.define(:version => 20130321124046) do
 
   create_table "contest_followings", :force => true do |t|
     t.integer  "contest_id"
